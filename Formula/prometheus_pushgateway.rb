39a1c7d1d48bde3d8c466f88b2de86bd9a2976ed
Aryan Raj <aryan.raj@coupa.com>
2023-11-09 21:15:59 +0530

    Added prometheus/pushgateway

---
+++ Formula/prometheus_pushgateway.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class PrometheusPushgateway
+  desc 'CLI to prometheus/pushgateway'
+  homepage 'https://github.com/prometheus/pushgateway'
+  url 'https://github.com/prometheus/pushgateway/releases/download/v1.6.2/pushgateway-1.6.2.darwin-arm64.tar.gz'
+  sha256 '07695d4c972fcc6aea0baadd00c5a753fec18ac5f6b8e58e51a49fef1c9cfce7'
+
+  def install
+    bin.install 'pushgateway'
+  end
+end
